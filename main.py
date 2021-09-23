7c68c0c74a3535e1f80828843e3de9d4712971f7
Matthew Vanek <47370185+MrVanek@users.noreply.github.com>
2021-09-23 14:01:44 +0000

    screen size

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
+wn = Screen()
+width = 500
+height = 500
+wn.setup(width,height)
 max = Turtle()
 max.shape("turtle")
-max.forward(500)
+max.forward(width/2-20)
 
 while True:
   pass
